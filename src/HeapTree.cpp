a2be2b3716d9166e414f2fd5c06f57c6675ba4cb
John <john@john-programmer.(none)>
2013-04-21 03:11:40 -0300

    tirando alguns itens desnecessários

--- src/HeapTree.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/HeapTree.h"
-template <> HeapTree<int>::HeapTree() {}
-
-int main(void){
-	HeapTree<int> Struct;
-	Struct.add(3);
-	cout << (int)Struct.remove(NULL);
-	return 0;
-}
